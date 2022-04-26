b6155b3cb511f5f70447464d230a3479a8c9486f
madisonlurae <benne2ml@dukes.jmu.edu>
2022-04-26 00:24:20 -0400

    attempt greeting 1

---
+++ javascript.js
@@ -0,0 +1,20 @@
+/*
+B1. A dynamic greeting - the website should greet your users with the phrase, "Good morning", "Good afternoon", or "Good evening" depending on the LOCAL time of your user.
+
+B2. Change in button style for the currently selected button (consider fadeIn, fadeOut or fadeToogle)
+
+B3. A menu that slides down or out from the Left or Top of the screen (slideDown, slideUp, slideToggle).
+*/
+
+var d = new Date();
+var time = d.getHours();
+
+if (time < 12) {
+  document.write("<b>Good morning!</b>");
+}
+if (time > 12) {
+  document.write("<b>Good afternoon!</b>");
+}
+if (time == 12) {
+  document.write("<b>Go eat lunch!</b>");
+}
