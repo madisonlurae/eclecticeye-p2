bf1a172667d210efd84ae09c2353dd1d58c5f729
madisonlurae <benne2ml@dukes.jmu.edu>
2022-04-29 16:19:51 -0400

    forgot the dot

--- javascript.js
+++ javascript.js
@@ -35,7 +35,7 @@ navButton.onmouseover = function () {
 };
 */
 (document).ready(function(){
-    ("nav-link").onmouseover(function(){
-        ("nav-link").fadeToggle();
+    (".nav-link").onmouseover(function(){
+        (".nav-link").fadeToggle();
     });
 });
