1dd3be88e56723df5e38d0d955da0fce3ac88a02
madisonlurae <benne2ml@dukes.jmu.edu>
2022-04-29 12:10:35 -0400

    alert once yuh. across all pages tho???

--- javascript.js
+++ javascript.js
@@ -6,14 +6,16 @@ B2. Change in button style for the currently selected button (consider fadeIn, f
 B3. A menu that slides down or out from the Left or Top of the screen (slideDown, slideUp, slideToggle).
 */
 
+//TIME OF DAY ALERT
 var d = new Date();
 var time = d.getHours();
 var alerted = localStorage.getItem('alerted') || '';
 
-//check if user has already recieved time alert
-//to ensure they don't recieve everytime they go to home page
+/*check if user has already recieved time alert
+to ensure they don't recieve everytime they go to home page*/
 if (alerted != 'yes') {
     localStorage.setItem('alerted','yes');
+    var dayAlerted = d;
     if (time < 12) {
         alert("Good morning!");
     }
@@ -23,43 +25,8 @@ if (alerted != 'yes') {
     if (time > 17) {
         alert("Good evening!");
     }
-}
-
-
-
-/*
-function createCookie(name, value, days) {
-    var expires;
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toGMTString();
+} else { //reset alert if it is a new day
+    if (dayAlerted != d) {
+        localStorage.setItem('alerted','no');
     }
-    else {
-        expires = "";
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
 }
-
-function getCookie(c_name) {
-    if (document.cookie.length > 0) {
-        c_start = document.cookie.indexOf(c_name + "=");
-        if (c_start != -1) {
-            c_start = c_start + c_name.length + 1;
-            c_end = document.cookie.indexOf(";", c_start);
-            if (c_end == -1) {
-                c_end = document.cookie.length;
-            }
-            return unescape(document.cookie.substring(c_start, c_end));
-        }
-    }
-    return "";
-
-}
-
-var alerted = getCookie('alerted') || '';
-if (alerted != 'yes') {
-    createCookie('alerted','yes',1);//cookies expires after 1 day
- }
-
-*/
