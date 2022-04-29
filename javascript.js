7e7c9627f5048fd4c1084e024a71571fca2a966a
madisonlurae <benne2ml@dukes.jmu.edu>
2022-04-29 11:52:09 -0400

    alert once???

--- javascript.js
+++ javascript.js
@@ -8,22 +8,56 @@ B3. A menu that slides down or out from the Left or Top of the screen (slideDown
 
 var d = new Date();
 var time = d.getHours();
-//var morning = alert("Good morning!");
 
 if (time < 12) {
-    alert(time);
-    //morning;
+    alert("Good morning!");
 }
 if (time > 12 & time < 17) {
     alert("Good afternoon!");
 }
 if (time > 17) {
     alert("Good evening!");
-/*
+}
+
 var alerted = localStorage.getItem('alerted') || '';
 if (alerted != 'yes') {
-  alert("My alert.");
-  localStorage.setItem('alerted','yes');
+    alert("My alert.");
+    localStorage.setItem('alerted','yes');
 }
+
+/*
+function createCookie(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+
+}
+
+var alerted = getCookie('alerted') || '';
+if (alerted != 'yes') {
+    createCookie('alerted','yes',1);//cookies expires after 1 day
+ }
+
 */
-}
