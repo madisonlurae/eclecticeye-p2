9be01b95e777b63275e7b36f14e61b165c045c1e
madisonlurae <benne2ml@dukes.jmu.edu>
2022-05-02 22:32:30 -0400

    host

--- php/connect-db.php
+++ php/connect-db.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-$host = "localhost";
+$host = "143.198.12.22";
 $user = "root";
-$password = "";
+$password = "pandabear";
 $dbname = "eclectic-eye-login";
 
 $connect = mysqli_connect($host, $user, $password,$dbname);
