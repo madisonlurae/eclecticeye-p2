a0808a209ed3f1d97fb17e21d7d2b38304e4c4c3
madisonlurae <benne2ml@dukes.jmu.edu>
2022-05-03 01:58:17 -0400

    underscore genius

--- php/connect-db.php
+++ php/connect-db.php
@@ -5,9 +5,9 @@ session_start();
 $host = "localhost";
 $user = "root";
 $password = "";
-$dbname = "eclectic-eye-login";
+$dbname = "eclectic_eye_login";
 
-$con = mysqli_connect($host, $user, $password,$dbname);
+$con = mysqli_connect($host, $user, $password, $dbname);
 //check connection
 if (!$con) {
   die("Connection failed");
